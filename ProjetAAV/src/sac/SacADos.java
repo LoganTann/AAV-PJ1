4fb5cd50058c9ecf53643320ab6c835fe4ff762f
LoganTann <logon313@hotmail.fr>
2021-09-16 18:29:06 +0200

    Ajout de la lecture de fichier

--- ProjetAAV/src/sac/SacADos.java
+++ ProjetAAV/src/sac/SacADos.java
@@ -13,6 +13,19 @@ public class SacADos {
 	
 	public SacADos(String chemin, float poidsMaximal) {
 		this.poidsMaximal = poidsMaximal;
+		//
+		Scanner sc= new Scanner(new File(chemin));
+		while (scanner.hasNext()) {
+			String line = scanner.nextLine();
+
+			String[] parsed = line.split(";");
+			String objectName = parsed[0];
+			float objectValue = Float.parseFloat(parsed[1]);
+			float objectPrice = Float.parseFloat(parsed[2]);
+
+			Objet object = new Objet(objectName, objectValue, objectPrice);
+			objets.add(object);
+		}
 	}
 	
 	public String toString() {
