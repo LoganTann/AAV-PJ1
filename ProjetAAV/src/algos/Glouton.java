5ca55633463e79cb199cd9e0666432eb3a972cca
LoganTann <logon313@hotmail.fr>
2021-09-24 14:34:55 +0200

    Réduction du code de l'algo glouton

--- ProjetAAV/src/algos/Glouton.java
+++ ProjetAAV/src/algos/Glouton.java
@@ -11,27 +11,24 @@ public class Glouton {
         float cumulWeight = 0;
 
         // calcul de (vi/pi) pour chaque objet
-        for(int i = 0; i < NB_OBJECTS; i++) {
-            Objet o = objects.get(i);
+        for (Objet o : objects) {
             o.setRatio(o.getValue() / o.getWeight());
         }
 
-        Comparator<Objet> objectsSortedByRatioDesc
-                = Comparator.comparing(
+        objects.sort(Comparator.comparing(
                 (Objet::getRatio), (o1, o2) -> {
                     return o2.compareTo(o1);
-                });
-
-        Collections.sort(objects, objectsSortedByRatioDesc);
+                })
+        );
 
         // selection et evaluation de possibilite de mise dans le sac
-        for(int i = 0; i < NB_OBJECTS; i++) {
+        for (Objet object : objects) {
             System.out.println(cumulWeight);
-            float weight = objects.get(i).getWeight();
-            if(cumulWeight + weight <= bag.getWeightMax()) {
+            float weight = object.getWeight();
+            if (cumulWeight + weight <= bag.getWeightMax()) {
                 cumulWeight += weight;
                 // ajout dans le sac
-                bag.add(objects.get(i));
+                bag.add(object);
             }
         }
     }
